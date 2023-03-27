25153ebdb9bd02198acef17159daa03b8d1c2fcc
alxvth <a.vieth@tudelft.nl>
2023-03-27 16:46:16 +0200

    Add Transformation plugin example

---
+++ ExampleTransformation/src/ExampleTransformationPlugin.cpp
@@ -0,0 +1,172 @@
+#include "ExampleTransformationPlugin.h"
+
+#include <PointData.h>
+
+#include <actions/PluginTriggerAction.h>
+
+#include <QtCore>
+#include <QDebug>
+
+#include <cmath>
+
+Q_PLUGIN_METADATA(IID "nl.BioVault.ExampleTransformationPlugin")
+
+using namespace hdps;
+using namespace hdps::util;
+
+const QMap<ExampleTransformationPlugin::Type, QString> ExampleTransformationPlugin::types = QMap<ExampleTransformationPlugin::Type, QString>({
+    { ExampleTransformationPlugin::Type::Abs, "Abs (in-place)" },
+    { ExampleTransformationPlugin::Type::Pow2, "Pow2 (derived data)" }
+    });
+
+
+ExampleTransformationPlugin::ExampleTransformationPlugin(const PluginFactory* factory) :
+    TransformationPlugin(factory),
+    _type(Type::Abs)
+{
+}
+
+void ExampleTransformationPlugin::transform()
+{
+    for (auto inputDataset : getInputDatasets()) {
+        auto points = Dataset<Points>(inputDataset);
+
+        if (!points.isValid())
+            continue;
+
+        points->getDataHierarchyItem().setTaskName("Transforming");
+        points->getDataHierarchyItem().setTaskRunning();
+        points->getDataHierarchyItem().setTaskDescription(QString("%1 transformation").arg(getTypeName(_type)));
+
+        switch (_type)
+        {
+        // Transform points in place
+        case ExampleTransformationPlugin::Type::Abs:
+        {
+            points->setLocked(true);
+
+            points->visitData([this, &points](auto pointData) {
+                std::uint32_t noPointsProcessed = 0;
+
+                for (auto point : pointData) {
+                    for (std::uint32_t dimensionIndex = 0; dimensionIndex < points->getNumDimensions(); dimensionIndex++) {
+                        point[dimensionIndex] = std::abs(point[dimensionIndex]);
+                    }
+
+                    ++noPointsProcessed;
+
+                    if (noPointsProcessed % 1000 == 0) {
+                        points->getDataHierarchyItem().setTaskProgress(static_cast<float>(noPointsProcessed) / static_cast<float>(points->getNumPoints()));
+
+                        QApplication::processEvents();
+                    }
+                }
+                });
+
+            points->setLocked(false);
+
+            events().notifyDatasetChanged(points);
+
+            break;
+        }
+        // Create new data set
+        case ExampleTransformationPlugin::Type::Pow2:
+        {
+            auto derivedData = _core->createDerivedDataset<Points>(points->getGuiName() + "(Pow2)", points);
+            events().notifyDatasetAdded(derivedData);
+
+            std::vector<float> transformedData;
+            transformedData.resize(points->getNumPoints() * points->getNumDimensions());
+
+            points->constVisitFromBeginToEnd([&transformedData](auto begin, auto end) {
+                std::uint32_t noItemsProcessed = 0;
+
+                for (auto it = begin; it != end; ++it) {
+                    transformedData[noItemsProcessed] = std::pow(*it, 2.0f);
+                    noItemsProcessed++;
+                }
+                });
+
+
+            derivedData->setData(transformedData.data(), points->getNumPoints(), points->getNumDimensions());
+            events().notifyDatasetChanged(derivedData);
+
+            break;
+        }
+        default:
+            break;
+        }
+
+        points->getDataHierarchyItem().setTaskProgress(1.0f);
+        points->getDataHierarchyItem().setTaskFinished();
+
+    }
+}
+
+
+ExampleTransformationPlugin::Type ExampleTransformationPlugin::getType() const
+{
+    return _type;
+}
+
+void ExampleTransformationPlugin::setType(const Type& type)
+{
+    if (type == _type)
+        return;
+
+    _type = type;
+}
+
+QString ExampleTransformationPlugin::getTypeName(const Type& type)
+{
+    return types[type];
+}
+
+ExampleTransformationPlugin* ExampleTransformationPluginFactory::produce()
+{
+    // Return a new instance of the example transformation plugin
+    return new ExampleTransformationPlugin(this);
+}
+
+hdps::DataTypes ExampleTransformationPluginFactory::supportedDataTypes() const
+{
+    DataTypes supportedTypes;
+
+    // This example transformation plugin is compatible with points datasets
+    supportedTypes.append(PointType);
+
+    return supportedTypes;
+}
+
+PluginTriggerActions ExampleTransformationPluginFactory::getPluginTriggerActions(const hdps::Datasets& datasets) const
+{
+    PluginTriggerActions pluginTriggerActions;
+
+    const auto numberOfDatasets = datasets.count();
+
+    if (PluginFactory::areAllDatasetsOfTheSameType(datasets, PointType)) {
+        if (numberOfDatasets >= 1 && datasets.first()->getDataType() == PointType) {
+            const auto addPluginTriggerAction = [this, &pluginTriggerActions, datasets](const ExampleTransformationPlugin::Type& type) -> void {
+                const auto typeName = ExampleTransformationPlugin::getTypeName(type);
+
+                auto pluginTriggerAction = new PluginTriggerAction(const_cast<ExampleTransformationPluginFactory*>(this), this, QString("Example/%1").arg(typeName), QString("Perform %1 data transformation").arg(typeName), getIcon(), [this, datasets, type](PluginTriggerAction& pluginTriggerAction) -> void {
+                    for (auto dataset : datasets) {
+                        auto pluginInstance = dynamic_cast<ExampleTransformationPlugin*>(plugins().requestPlugin(getKind()));
+
+                        pluginInstance->setInputDatasets(datasets);
+                        pluginInstance->setType(type);
+                        pluginInstance->transform();
+                    }
+                    });
+
+                pluginTriggerActions << pluginTriggerAction;
+            };
+
+            addPluginTriggerAction(ExampleTransformationPlugin::Type::Abs);
+            addPluginTriggerAction(ExampleTransformationPlugin::Type::Pow2);
+        }
+    }
+
+    return pluginTriggerActions;
+}
+
---
+++ ExampleTransformation/src/ExampleTransformationPlugin.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <TransformationPlugin.h>
+#include <actions/StringAction.h>
+
+/** All plugin related classes are in the HDPS plugin namespace */
+using namespace hdps::plugin;
+using namespace hdps::gui;
+
+/**
+ * Example transformation plugin class
+ *
+ * This transformation plugin class provides skeleton code that shows how to develop 
+ * an transformation plugin in HDPS.
+ * 
+ * In contrast to analysis plugins, transformation do not create an output data set by default,
+ * but operate on the input data set. In this example, we provide to transformation options,
+ * either simply taking the absolute value of each data point or raising it to the power of 2.
+ *
+ * To see the plugin in action, please follow the steps below:
+ *
+ * 1. This plugin works on points datasets and is created by right-clicking a points
+ * dataset in the data hierarchy viewer and choosing Transform >> Example transformation.
+ * 2. Chose the transformation option
+ * 
+ */
+class ExampleTransformationPlugin : public TransformationPlugin
+{
+Q_OBJECT
+
+public:
+
+    /** Define the transformation options */
+    enum class Type {
+        Abs,       /** abosulte value */
+        Pow2       /** value squared */
+    };
+
+    static const QMap<Type, QString> types;
+
+public:
+
+    /**
+     * Constructor
+     * @param factory Pointer to the plugin factory
+     */
+    ExampleTransformationPlugin(const PluginFactory* factory);
+
+    /** Destructor */
+    ~ExampleTransformationPlugin() override = default;
+
+    /** Initialization is called when the plugin is first instantiated. */
+    void init() override {};
+
+    /** Performs the data transformation */
+    void transform() override;
+
+    /**
+     * Get transformation type
+     * @return transformation type
+     */
+    Type getType() const;
+
+    /**
+     * Set transformation type
+     * @param type transformation type
+     */
+    void setType(const Type& type);
+
+    /**
+     * Get string representation of transformation type enum
+     * @param type transformation type
+     * @return Type name
+     */
+    static QString getTypeName(const Type& type);
+
+private:
+    Type    _type;      /** Data conversion type */
+
+};
+
+/**
+ * Example transform plugin factory class
+ *
+ * Note: Factory does not need to be altered (merely responsible for generating new plugins when requested)
+ */
+class ExampleTransformationPluginFactory : public TransformationPluginFactory
+{
+    Q_INTERFACES(hdps::plugin::TransformationPluginFactory hdps::plugin::PluginFactory)
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID   "nl.BioVault.ExampleTransformationPlugin"
+                      FILE  "ExampleTransformationPlugin.json")
+
+public:
+
+    /** Default constructor */
+    ExampleTransformationPluginFactory() {}
+
+    /** Destructor */
+    ~ExampleTransformationPluginFactory() override {}
+
+    /** Creates an instance of the example transform plugin */
+    ExampleTransformationPlugin* produce() override;
+
+    /** Returns the data types that are supported by the example transformation plugin */
+    hdps::DataTypes supportedDataTypes() const override;
+
+    /** Enable right-click on data set to execute transformation */
+    PluginTriggerActions getPluginTriggerActions(const hdps::Datasets& datasets) const override;
+
+};

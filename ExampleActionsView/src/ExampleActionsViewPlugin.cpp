5539aa2ff7ab944e013205eb4adcd92dfd717ea9
Thomas Kroes <t.kroes@lumc.nl>
2023-06-08 16:06:11 +0200

    Work on example actions view

--- ExampleActionsView/src/ExampleActionsViewPlugin.cpp
+++ ExampleActionsView/src/ExampleActionsViewPlugin.cpp
@@ -8,8 +8,11 @@
 #include <actions/ColorAction.h>
 #include <actions/ColorMapAction.h>
 #include <actions/DecimalAction.h>
-#include <actions/DecimalRangeAction.h>
 #include <actions/IntegralAction.h>
+#include <actions/DecimalRangeAction.h>
+#include <actions/IntegralRangeAction.h>
+#include <actions/DecimalRectangleAction.h>
+#include <actions/IntegralRectangleAction.h>
 #include <actions/OptionAction.h>
 #include <actions/OptionsAction.h>
 #include <actions/StringAction.h>
@@ -20,6 +23,9 @@
 #include <actions/ImageAction.h>
 #include <actions/VersionAction.h>
 #include <actions/WindowLevelAction.h>
+#include <actions/HorizontalGroupAction.h>
+#include <actions/VerticalGroupAction.h>
+#include <actions/HorizontalToolbarAction.h>
 
 #include <PointData/DimensionPickerAction.h>
 #include <PointData/DimensionsPickerAction.h>
@@ -44,34 +50,119 @@ void ExampleActionsViewPlugin::init()
 
     auto groupAction = new GroupAction(this, "Group");
 
-    auto addAction = [this, groupAction](WidgetAction* action) -> void {
-        action->setConnectionPermissionsToAll();
+    //groupAction->setShowLabels(false);
+
+    auto addAction = [this, groupAction](WidgetAction* action, bool setConnectionPermissionsToAll = true, std::int32_t defaultWidgetFlags = -1) -> void {
+        if (setConnectionPermissionsToAll)
+            action->setConnectionPermissionsToAll();
         
+        if (defaultWidgetFlags >= 0)
+            action->setDefaultWidgetFlags(defaultWidgetFlags);
+
         _actions << action;
     };
 
-    addAction(new ToggleAction(this, "Toggle"));
-    addAction(new TriggerAction(this, "Trigger"));
-    addAction(new ColorAction(this, "Color"));
-    addAction(new ColorMapAction(this, "ColorMap"));
-    addAction(new DecimalAction(this, "Decimal"));
-    addAction(new DecimalRangeAction(this, "Decimal Range"));
-    addAction(new IntegralAction(this, "Integral"));
-    addAction(new OptionAction(this, "Option"));
-    addAction(new OptionsAction(this, "Options"));
-    addAction(new StringAction(this, "String"));
-    addAction(new StringsAction(this, "Strings"));
-    addAction(new DirectoryPickerAction(this, "Directory"));
-    addAction(new FilePickerAction(this, "File"));
-    addAction(new DatasetPickerAction(this, "Dataset"));
-    addAction(new ImageAction(this, "Image"));
-    addAction(new VersionAction(this));
-    addAction(new WindowLevelAction(this));
-    addAction(new DimensionPickerAction(this, "Dimension"));
-    addAction(new DimensionsPickerAction(this));
+    auto toggleAction               = new ToggleAction(this, "Toggle");
+    auto triggerAction              = new TriggerAction(this, "Trigger");
+    auto colorAction                = new ColorAction(this, "Color");
+    auto colorMapAction             = new ColorMapAction(this, "ColorMap");
+    auto decimalAction              = new DecimalAction(this, "Decimal");
+    auto integralAction             = new IntegralAction(this, "Integral");
+    auto decimalRangeAction         = new DecimalRangeAction(this, "Decimal Range");
+    auto integralRangeAction        = new IntegralRangeAction(this, "Integral Range");
+    auto decimalRectangleAction     = new DecimalRectangleAction(this, "Decimal Rectangle");
+    auto integralRectangleAction    = new IntegralRectangleAction(this, "Integral Rectangle");
+    auto optionAAction              = new OptionAction(this, "Option A", { "A", "B", "C" });
+    auto optionBAction              = new OptionAction(this, "Option B", { "C", "D", "E" });
+    auto optionsAAction             = new OptionsAction(this, "Options A", { "A", "B", "C" });
+    auto optionsBAction             = new OptionsAction(this, "Options B", { "B", "C", "D" });
+    auto stringAction               = new StringAction(this, "String");
+    auto stringsAction              = new StringsAction(this, "Strings");
+    auto datasetPickerAction        = new DatasetPickerAction(this, "Dataset");
+    auto dimensionPickerAction      = new DimensionPickerAction(this, "Dimension");
+    auto dimensionsPickerAction     = new DimensionsPickerAction(this, "Dimensions");
+    auto imageAction                = new ImageAction(this, "Image");
+    auto windowLevelAction          = new WindowLevelAction(this, "Window/level");
+    auto versionAction              = new VersionAction(this, "Version");
+    auto directoryPickerAction      = new DirectoryPickerAction(this, "Directory");
+    auto filePickerAction           = new FilePickerAction(this, "File");
+    auto horizontalGroupAction      = new HorizontalGroupAction(this, "Horizontal group");
+    auto verticalGroupAction        = new VerticalGroupAction(this, "Vertical group");
+    auto horizontalToolbarAction    = new HorizontalToolbarAction(this, "Horizontal Toolbar");
 
-    groupAction->setActions(_actions);
+    optionAAction->setPlaceHolderString("Select option");
+    optionBAction->setPlaceHolderString("Select option");
 
+    connect(datasetPickerAction, &DatasetPickerAction::datasetPicked, this, [this, dimensionPickerAction, dimensionsPickerAction](Dataset<DatasetImpl> dataset) -> void {
+        dimensionPickerAction->setPointsDataset(Dataset<Points>(dataset));
+        dimensionsPickerAction->setPointsDataset(Dataset<Points>(dataset));
+    });
+
+    horizontalGroupAction->setShowLabels(false);
+    horizontalGroupAction->addAction(optionAAction);
+    horizontalGroupAction->addAction(stringAction);
+
+    verticalGroupAction->addAction(optionAAction);
+    verticalGroupAction->addAction(stringAction);
+
+    auto vectorTypeAction   = new OptionAction(this, "Vector Type", { "2D", "3D" });
+    auto vectorXAction      = new DecimalAction(this, "X");
+    auto vectorYAction      = new DecimalAction(this, "Y");
+    auto vectorAction       = new HorizontalGroupAction(this, "Vector");
+
+    const auto vectorTypeChanged = [vectorTypeAction, vectorYAction]() -> void {
+        vectorYAction->setVisible(vectorTypeAction->getCurrentText() == "3D");
+    };
+
+    vectorTypeChanged();
+
+    connect(vectorTypeAction, &OptionAction::currentIndexChanged, this, vectorTypeChanged);
+
+    vectorAction->addAction(vectorTypeAction);
+    vectorAction->addAction(vectorXAction);
+    vectorAction->addAction(vectorYAction);
+
+    colorAction->setConfigurationFlag(WidgetAction::ConfigurationFlag::ForceCollapsedInGroup);
+    decimalAction->setConfigurationFlag(WidgetAction::ConfigurationFlag::ForceCollapsedInGroup);
+
+    horizontalToolbarAction->addAction(vectorAction, 1);
+    horizontalToolbarAction->addAction(colorAction, 3);
+    horizontalToolbarAction->addAction(colorMapAction, 8);
+    horizontalToolbarAction->addAction(decimalAction, 1);
+
+    addAction(toggleAction);
+    addAction(triggerAction);
+    addAction(colorAction);
+    addAction(colorMapAction);
+    addAction(decimalAction);
+    addAction(integralAction);
+    addAction(decimalRangeAction);
+    addAction(integralRangeAction);
+    addAction(decimalRectangleAction);
+    addAction(integralRectangleAction);
+    addAction(optionAAction);
+    addAction(optionBAction);
+    addAction(optionsAAction);
+    addAction(optionsBAction);
+    addAction(stringAction);
+    addAction(stringsAction);
+    addAction(datasetPickerAction, true, OptionAction::ComboBox | OptionAction::Clearable);
+    addAction(dimensionPickerAction);
+    addAction(dimensionsPickerAction);
+    addAction(imageAction, false);
+    addAction(windowLevelAction);
+    addAction(versionAction);
+    addAction(directoryPickerAction, false);
+    addAction(filePickerAction, false);
+    addAction(horizontalGroupAction, false);
+    addAction(verticalGroupAction, false);
+    addAction(horizontalToolbarAction, false);
+
+    for (auto action : _actions)
+        groupAction->addAction(action);
+
+    layout->setAlignment(Qt::AlignLeft);
+    //layout->addWidget(horizontalToolbarAction->createWidget(&getWidget()));
     layout->addWidget(groupAction->createWidget(&getWidget()));
     layout->addStretch(1);
 

25e923db981b247d431ae50bc3202ed179d8c9b6
Alexander Vieth <a.vieth@tudelft.nl>
2023-06-22 17:44:19 +0200

    Clean up

--- ExampleViewJS/res/example_chart/radar_chart.tools.js
+++ ExampleViewJS/res/example_chart/radar_chart.tools.js
@@ -1,18 +1,4 @@
 var data = [];                  // JSON data
-var chart = RadarChart.chart(); // char object
-var cfg = chart.config();       // retrieve default config
-chart.config({
-    containerClass: 'radar-chart',
-    w: window.innerWidth,
-    h: window.innerHeight,
-    factor: 0.95,
-});
-
-var svg = d3.select("div#container")
-  .append("svg")
-  .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", "0 0 " + window.innerWidth + " " + window.innerHeight)
-  .classed("svg-content", true);
 
 // parses JSON string data and radar plot
 function setChartData(d) {
@@ -28,11 +14,23 @@ function drawChart() {
 
     log("drawChart")
 
-    // svg.selectAll('*').remove();
-    svg.append('g').classed('single', 1).datum(data).call(chart);  
-}
+    // remove possible old chart
+    d3.select("div#container").select("svg").remove();
+    
+    // config new chart
+    var chart = RadarChart.chart(); // char object
+    chart.config({
+        containerClass: 'radar-chart',
+        w: window.innerWidth,
+        h: window.innerHeight,
+    });
 
-// highlights selection from qt
-function setSelectionIDsFromQt(IDs) {
-
+    // plot chart in auto-resizable box
+    d3.select("div#container")
+      .append("svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 " + window.innerWidth + " " + window.innerHeight)
+      .classed("svg-content", true)
+      .datum(data)
+      .call(chart);
 }
--- ExampleViewJS/res/js_libs/qwebchannel.tools.js
+++ ExampleViewJS/res/js_libs/qwebchannel.tools.js
@@ -1,28 +1,6 @@
-// find out if the QtBridge is available
 var isQtAvailable = false;
-try {
-    new QWebChannel(qt.webChannelTransport, function (channel) {
-        // Establish connection
-        QtBridge = channel.objects.QtBridge;
 
-        // register signals 
-        QtBridge.qt_js_setDataInJS.connect(function () { setChartData(arguments[0]); });
-        QtBridge.qt_js_setSelectionInJS.connect(function () { setSelectionIDsFromQt(arguments[0]); });
-
-        // confirm successfull connection
-        isQtAvailable = true;
-        notifyBridgeAvailable();
-    });
-} catch (error) {
-	log("ExampleViewJSPlugin: qwebchannel: could not connect qt");
-}
-
-// pipe errors to log
-window.onerror = function (msg, url, num) {
-    log("ExampleViewJSPlugin: qwebchannel: Error: " + msg + "\nURL: " + url + "\nLine: " + num);
-};
-
-// auto log for Qt and console
+// utility function: auto log for Qt and console
 function log(logtext) {
 
 	if (isQtAvailable) {
@@ -33,6 +11,24 @@ function log(logtext) {
 	}
 }
 
+// find out if the QtBridge is available
+try {
+    new QWebChannel(qt.webChannelTransport, function (channel) {
+        // Establish connection
+        QtBridge = channel.objects.QtBridge;
+
+        // register signals 
+        QtBridge.qt_js_setDataInJS.connect(function () { setChartData(arguments[0]); });
+
+        // confirm successfull connection
+        isQtAvailable = true;
+        notifyBridgeAvailable();
+    });
+} catch (error) {
+	log("ExampleViewJSPlugin: qwebchannel: could not connect qt");
+}
+
+// 
 function notifyBridgeAvailable() {
 
     if (isQtAvailable) {
@@ -44,23 +40,15 @@ function notifyBridgeAvailable() {
 
 }
 
+// 
 function passSelectionToQt(dat) {
     if (isQtAvailable) {
         QtBridge.js_passSelectionToQt(dat);
 	}
 }
 
-function setViewBusy() {
-
-    if (isQtAvailable) {
-        QtBridge.js_viewIsBusy(true);
-    }
-}
-
-function setViewAvailable() {
-
-    if (isQtAvailable) {
-        QtBridge.js_viewIsBusy(false);
-    }
-}
+// utility function: pipe errors to log
+window.onerror = function (msg, url, num) {
+    log("ExampleViewJSPlugin: qwebchannel: Error: " + msg + "\nURL: " + url + "\nLine: " + num);
+};
 

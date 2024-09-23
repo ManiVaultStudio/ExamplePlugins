ae282b911f2ed2061f16c8ff9319ac0a94527343
Alexander Vieth <vieth.alexander@gmx.net>
2024-09-23 10:26:59 +0200

    Install vcpkg dependencies with github actions

--- conanfile.py
+++ conanfile.py
@@ -115,9 +115,29 @@ class ExamplePluginsConan(ConanFile):
         # Set some build options
         tc.variables["MV_UNITY_BUILD"] = "ON"
         
-        tc.variables["CMAKE_TOOLCHAIN_FILE"] = os.environ.get("VCPKG_ROOT", None) + '/scripts/buildsystems/vcpkg.cmake/'
-        tc.variables["CMAKE_PROJECT_INCLUDE"] = "build/conan_toolchain.cmake"
-        
+        # Install vcpkg dependencies
+        vcpkg_dir = os.environ["VCPKG_ROOT"]
+        vcpkg_exe = os.path.join(vcpkg_dir, "vcpkg.exe") if self.settings.os == "Windows" else os.path.join(vcpkg_dir, "vcpkg")
+        vcpkg_tc  = os.path.join(vcpkg_dir, "scripts", "buildsystems", "vcpkg.cmake")
+
+        vcpkg_triplet = "x64-windows"
+        if self.settings.os == "Macos":
+            vcpkg_triplet = "x64-osx"
+        if self.settings.os == "Linux":
+            vcpkg_triplet = "x64-linux"
+
+        print("vcpkg_dir: ", vcpkg_dir)
+        print("vcpkg_exe: ", vcpkg_exe)
+        print("vcpkg_tc: ", vcpkg_tc)
+        print("vcpkg_triplet: ", vcpkg_triplet)
+
+        tc.variables["VCPKG_LIBRARY_LINKAGE"]   = "dynamic"
+        tc.variables["VCPKG_TARGET_TRIPLET"]    = vcpkg_triplet
+        tc.variables["VCPKG_HOST_TRIPLET"]      = vcpkg_triplet
+        tc.variables["VCPKG_ROOT"]              = vcpkg_dir
+
+        tc.variables["CMAKE_PROJECT_INCLUDE"] = vcpkg_tc
+
         tc.generate()
 
     def _configure_cmake(self):

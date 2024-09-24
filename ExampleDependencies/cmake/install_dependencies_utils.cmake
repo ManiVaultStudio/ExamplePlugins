18d79735580b78fb50c29a810028221c4611e8ee
Alexander Vieth <vieth.alexander@gmx.net>
2024-09-24 09:34:55 +0200

    Move install command to function

--- ExampleDependencies/cmake/install_dependencies_utils.cmake
+++ ExampleDependencies/cmake/install_dependencies_utils.cmake
@@ -10,4 +10,15 @@ function(install_dependencies main_target)
     configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/install_dependencies.cmake.in" "${CMAKE_CURRENT_SOURCE_DIR}/cmake/install_dependencies.cmake" @ONLY)
 
     include(cmake/install_dependencies.cmake)
+
+    add_custom_command(TARGET ${main_target} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E echo "Installing: Plugin dependencies"
+    COMMAND "${CMAKE_COMMAND}"
+        --install ${CMAKE_CURRENT_BINARY_DIR}
+        --config $<CONFIGURATION>
+        --component PLUGIN_DEPENDENCIES
+        --prefix ${ManiVault_INSTALL_DIR}/$<CONFIGURATION>
+        --verbose
+    )
+
 endfunction()

4f844ca07333d8a11ac3f0f9edc1f5f04fc95be0
Thomas Kroes <t.kroes@lumc.nl>
2025-02-05 16:40:56 +0100

    Add example notifications (#38)

--- ExampleTransformation/src/ExampleTransformationPlugin.cpp
+++ ExampleTransformation/src/ExampleTransformationPlugin.cpp
@@ -22,6 +22,18 @@ ExampleTransformationPlugin::ExampleTransformationPlugin(const PluginFactory* fa
     TransformationPlugin(factory),
     _type(Type::Abs)
 {
+    addNotification("ManiVault Studio features a notification system that can be triggered from:<br>"
+        "<table>"
+        "<tr>"
+        "<th style='text-align: left;'>Within a plugin:  </th>"
+        "<td><a href='https://github.com/ManiVaultStudio/core/blob/3088ad09de6b5a5f3e84b24ffba44294bbb8001d/ManiVault/src/Plugin.h#L170'>addNotification(...)</a></td>"
+        "</tr>"
+        "<tr>"
+        "<th style='text-align: left;'>The help manager:  </th>"
+        "<td><a href='https://github.com/ManiVaultStudio/core/blob/126bd905d64d9b795dce188a06e9321eef132bd3/ManiVault/src/AbstractHelpManager.h#L100'>mv::help().addNotification(...)</a></td>"
+        "</tr>"
+        "</table>"
+    );
 }
 
 void ExampleTransformationPlugin::transform()
--- ExampleViewJS/src/ExampleViewJSPlugin.cpp
+++ ExampleViewJS/src/ExampleViewJSPlugin.cpp
@@ -100,6 +100,19 @@ void ExampleViewJSPlugin::init()
 
     // Create data so that we do not need to load any in this example
     createData();
+
+    addNotification("ManiVault Studio features a notification system that can be triggered from:<br>"
+        "<table>"
+        "<tr>"
+        "<th style='text-align: left;'>Within a plugin:  </th>"
+        "<td><a href='https://github.com/ManiVaultStudio/core/blob/3088ad09de6b5a5f3e84b24ffba44294bbb8001d/ManiVault/src/Plugin.h#L170'>addNotification(...)</a></td>"
+        "</tr>"
+        "<tr>"
+        "<th style='text-align: left;'>The help manager:  </th>"
+        "<td><a href='https://github.com/ManiVaultStudio/core/blob/126bd905d64d9b795dce188a06e9321eef132bd3/ManiVault/src/AbstractHelpManager.h#L100'>mv::help().addNotification(...)</a></td>"
+        "</tr>"
+        "</table>"
+    );
 }
 
 void ExampleViewJSPlugin::loadData(const mv::Datasets& datasets)

e5b20326077dae70efe9cfeed4b3c5d48ec01714
Alexander Vieth <vieth.alexander@gmx.net>
2026-07-24 11:16:29 +0200

    set to RelWithDebInfo

--- conanfile.py
+++ conanfile.py
@@ -126,8 +126,7 @@ class ExamplePluginsConan(ConanFile):
 
             tc.variables["CMAKE_PROJECT_INCLUDE"]   = vcpkg_tc.as_posix()
 
-            tc.cache_variables["CMAKE_MAP_IMPORTED_CONFIG_RELWITHDEBINFO"]    = "Release"
-            tc.cache_variables["CMAKE_MAP_IMPORTED_CONFIG_MINSIZEREL"]        = "Release"
+            tc.cache_variables["CMAKE_MAP_IMPORTED_CONFIG_MINSIZEREL"]        = "RelWithDebInfo"
 
         tc.generate()
 

20a0f7895de6adbbd1b4dc6c90e14f3281551994
Alexander Vieth <vieth.alexander@gmx.net>
2026-07-24 10:48:11 +0200

    Map configs

--- conanfile.py
+++ conanfile.py
@@ -126,6 +126,9 @@ class ExamplePluginsConan(ConanFile):
 
             tc.variables["CMAKE_PROJECT_INCLUDE"]   = vcpkg_tc.as_posix()
 
+            tc.variables["CMAKE_MAP_IMPORTED_CONFIG_RELWITHDEBINFO"]    = "Release"
+            tc.variables["CMAKE_MAP_IMPORTED_CONFIG_MINSIZEREL"]        = "Release"
+
         tc.generate()
 
     def _configure_cmake(self):
